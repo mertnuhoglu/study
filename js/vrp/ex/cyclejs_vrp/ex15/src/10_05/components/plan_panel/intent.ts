6bfcbc57f5b5d8165a9626853baad8569eabbcb8
Mert Nuhoglu <mert.nuhoglu@gmail.com>
2018-10-10 09:49:37 +0300

    vrp: filtering purchase_orders

--- js/vrp/ex/cyclejs_vrp/ex15/src/10_05/components/plan_panel/intent.ts
+++ js/vrp/ex/cyclejs_vrp/ex15/src/10_05/components/plan_panel/intent.ts
@@ -13,14 +13,19 @@ export function intent(dom$: DOMSource): Intent {
         category: 'purchase_order',
     }
     const planDetails$: xs<RequestInfo> = dom$.select('.get_plan_details').events('click')
-        .map((e) => ({ type: "GET_PLAN_DETAILS", payload: e.target.dataset.plan_id }))
-        .debug(x => { console.log("PlanPanel getPlanDetails$"); console.log(x); })
-        .mapTo(request)
-    // addListenerStream(planDetails$, "getPlanDetails$")
+			.map((e) => e.target.dataset.plan_id)
+			.debug(x => { console.log("PlanPanel getPlanDetails$"); console.log(x); })
+			.map(plan_id => ({
+				url: `http://localhost:8080/rest/purchase_order?purchase_order_id=eq.${plan_id}`,
+				method: 'GET',
+				headers: {
+						"Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoxLCJyb2xlIjoid2VidXNlciJ9.uSsS2cukBlM6QXe4Y0H90fsdkJSGcle9b7p_kMV1Ymk"
+				},
+				category: 'purchase_order',
+			}))
     const requests$: xs<RequestInfo> = xs.from([
             request,
         ])
         .debug(x => { console.log("PlanPanel intent requests$"); console.log(x); })
-    // const actions = { planDetails$ }
     return { requests$: planDetails$ }
 }
