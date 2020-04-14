acd2629a7ad176c7a2b1effa91706a2c31348970
Mert Nuhoglu <mert.nuhoglu@gmail.com>
2020-04-14 16:42:49 +0300

    studying: clojure by example on cursive

--- clj/ex/study_clojure/cljs_deps_edn01/src/core.clj
+++ clj/ex/study_clojure/cljs_deps_edn01/src/core.clj
@@ -1,1 +1,34 @@
-(ns core)
+(ns core
+  (:require [clojure.string :as str]))
+
+(defn sq [a]
+  (* a (+ 3 a)))
+
+(sq 2)
+
+true
+\H
+#".*"
+(#".*")
+:keyw
+:key-weird?#+-
+[2 3 5]
+{:a 1 :b 2}
+
+(str/trim " ali vx ")
+
+(def names (slurp "/usr/share/dict/propernames"))
+
+(def names2
+  (str/split-lines
+    (slurp "/usr/share/dict/propernames")))
+
+(:some-thing)
+
+(ali-veli)
+
+(count
+  (map name [:a :b :c]))
+
+(let [x 10 y 20]
+  test)
--- clj/ex/study_clojure/ex06/src/clojure_by_example_kimh.clj
+++ clj/ex/study_clojure/ex06/src/clojure_by_example_kimh.clj
@@ -1,1 +1,67 @@
-(ns clojure_by_example_kimh)
+(ns clojure_by_example_kimh
+  (:require [clojure.string :as str]))
+
+; Most codes taken from https://github.com/kimh/clojure-by-example/blob/master/source/index.html.md
+
+(println "merhaba")
+
+"h"
+
+true
+
+(str "each " "line " "is a" " form")
+
+(str "forms are " "expressions")
+
+(def a-binding "is an assignment in other languages")
+
+(def bindings "map a name to a value")
+
+(type 'quoted-symbols-are-not-evaluated)
+
+(unresolved-symbol-error)
+
+(let [binds "a value to a local name"] (str binds))
+
+(let [a 10
+      b 20] (+ 10 20))
+
+; narrow indentation
+(let
+  [a 10
+   b 20]
+  (+ 10 20))
+
+; clojure style convention
+(let [a 10
+      b 20]
+  (+ 10 20))
+
+(def def-bindings "are global")
+
+(defn a-function
+  [name]
+  (str name))
+
+(defn documented
+  "This is documentation"
+  []
+  (str ""))
+
+(meta (var documented))
+(meta #'documented)
+
+(fn [] (str ""))
+(def anon-function (fn [] (str "")))
+(anon-function)
+#(str "anon function")
+(#(str "evaluate " "anon function"))
+#(str "function argument: " %)
+(#(str "function argument: " %) "arg value")
+(let [multiple-args #(+ %1 %2)]
+  (multiple-args 50 20))
+
+(defn higher-order-function [fun]
+  (fun 10))
+(higher-order-function inc)
+
