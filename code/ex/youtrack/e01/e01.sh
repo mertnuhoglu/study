9308b8aba13d8451b4b6e0d1781675f4b009996f
Mert Nuhoglu <mert.nuhoglu@gmail.com>
2022-02-03 21:45:34 +0300

    youtrack api from R

---
+++ code/ex/youtrack/e01/e01.sh
@@ -0,0 +1,5 @@
+curl -X GET 'https://example.youtrack.cloud/api/admin/users/me' \
+-H 'Accept: application/json' \
+-H 'Authorization: Bearer perm:am9obi5kb2U=.UG9zdG1hbiBKb2huIERvZQ==.jJe0eYhhkV271j1lCpfknNYOEakNk7' \
+-H 'Cache-Control: no-cache'
+
---
+++ code/ex/youtrack/e01/e02.sh
@@ -0,0 +1,6 @@
+curl -X GET 'https://youtrack.layermark.com/api/users/me' \
+-H 'Accept: application/json' \
+-H "Authorization: Bearer perm:${YOUTRACK_AUTH}" \
+-H 'Cache-Control: no-cache'
+
+
