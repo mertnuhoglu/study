bed61f88ca89903cc8708b5be047db927323f0b0
Mert Nuhoglu <mert.nuhoglu@gmail.com>
2023-03-01 11:31:31 +0300

    clj: 20230301: shadowcljs projeleri

---
+++ clj/ex/shadowcljs/shadowcljs-02/src/main/mert/frontend/app.cljs
@@ -0,0 +1,13 @@
+(ns mert.frontend.app)
+
+(defn init []
+  (println "Hello World3"))
+
+; (+ 1 2)
+; (js/alert "hi")
+; (namespace ::x)
+; (ns cljs.user)
+; (namespace ::x)
+; (require 'js)
+; (shadow/repl :frontend)
+; (js/alert "hi")
